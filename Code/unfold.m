71f43f15a300cf25be3e283e06a1b23dbb9ab619
davidivanis <133990750+davidivanis@users.noreply.github.com>
2023-05-20 01:01:34 +0200

    Rename unfold.m to Code/unfold.m

--- Code/unfold.m
+++ Code/unfold.m
@@ -3,7 +3,7 @@ function M = unfold( T, mode )
 %
 % Dio nastavnog materijala na  
 % PMF-Matematicki odsjek, Sveuciliste u Zagrebu
-% Diplomski kolegij Uvod u slozeno pretrazivanje podataka (© Zlatko Drmac)
+% Diplomski kolegij Uvod u slozeno pretrazivanje podataka (Â© Zlatko Drmac)
 %
 % Ulazni parametri
 % ================
